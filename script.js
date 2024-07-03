56e99fe9622a1334b39524af8d276b69cbdb22ee
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-07-03 12:22:27 +0000

    add ui structure

--- script.js
+++ script.js
@@ -7,52 +7,52 @@ const PI = 3.14159;
 
 
 
-isNotValid = true;
-while (isNotValid) {
-  const roomShape = prompt("What shape is your room? R = round, S = square, L = L-shaped").toLowerCase();
-  if (roomShape == "r") {
-    const roomRadius = parseInt(prompt("What is the radius of your room?"));
-    if (roomRadius > 0 && !isNaN(roomRadius)) {
-      const area = PI * Math.pow(roomRadius, 2);
-      const gallons = area / 350;
-      isNotValid = false;
-      alert(`You will need ${Math.ceil(gallons)} gallon(s) of paint to cover ${area.toFixed(2)} square feet.`);
-    } else {
-      alert("Invalid input. Please enter a positive number for the radius.");
-    }
-  } else if (roomShape == "l") {
-    const length1 = parseInt(prompt("What is the length of the first rectangle?"))
-    const width1 = parseInt(prompt("What is the width of the first rectangle?"));
-    const length2 = parseInt(prompt("What is the length of the second rectangle?"));
-    const width2 = parseInt(prompt("What is the width of the second rectangle?"));
-    if (length1 > 0 && width1 > 0 && length2 > 0 && width2 > 0 && !isNaN(length1) && !isNaN(width1) && !isNaN(length2)) {
-      const area1 = length1 * width1;
-      const area2 = length2 * width2;
-      const area = area1 + area2;
-      gallons = area / 350;
-      isNotValid = false;
-      alert(`You will need ${Math.ceil(gallons)} gallons of paint to cover ${area.toFixed(2)} square feet.`);
-    } else {
-      alert("Invaild input. Please enter positive numbers for the length and width of the rectangles.");
-    }
+// isNotValid = true;
+// while (isNotValid) {
+//   const roomShape = prompt("What shape is your room? R = round, S = square, L = L-shaped").toLowerCase();
+//   if (roomShape == "r") {
+//     const roomRadius = parseInt(prompt("What is the radius of your room?"));
+//     if (roomRadius > 0 && !isNaN(roomRadius)) {
+//       const area = PI * Math.pow(roomRadius, 2);
+//       const gallons = area / 350;
+//       isNotValid = false;
+//       alert(`You will need ${Math.ceil(gallons)} gallon(s) of paint to cover ${area.toFixed(2)} square feet.`);
+//     } else {
+//       alert("Invalid input. Please enter a positive number for the radius.");
+//     }
+//   } else if (roomShape == "l") {
+//     const length1 = parseInt(prompt("What is the length of the first rectangle?"))
+//     const width1 = parseInt(prompt("What is the width of the first rectangle?"));
+//     const length2 = parseInt(prompt("What is the length of the second rectangle?"));
+//     const width2 = parseInt(prompt("What is the width of the second rectangle?"));
+//     if (length1 > 0 && width1 > 0 && length2 > 0 && width2 > 0 && !isNaN(length1) && !isNaN(width1) && !isNaN(length2)) {
+//       const area1 = length1 * width1;
+//       const area2 = length2 * width2;
+//       const area = area1 + area2;
+//       gallons = area / 350;
+//       isNotValid = false;
+//       alert(`You will need ${Math.ceil(gallons)} gallons of paint to cover ${area.toFixed(2)} square feet.`);
+//     } else {
+//       alert("Invaild input. Please enter positive numbers for the length and width of the rectangles.");
+//     }
 
 
 
-  } else if (roomShape == "s" && isNaN(roomShape)) {
-    const length = parseInt(prompt("Enter length of room in feet: "));
-    const width = parseInt(prompt("Enter width of room in feet: "));
-    if (length > 0 && width > 0 && !isNaN(length) && !isNaN(width)) {
-      const area = length * width;
-      const gallons = area / 350;
-      isNotVailad = false;
-      alert(`You will need ${Math.ceil(gallons)} gallons of paint to cover ${area.toFixed(2)} square feet.`);
-    } else {
-      alert("Invalid input. Please enter positive numbers for the lengths and widths of the rectangles.");
-    }
+//   } else if (roomShape == "s" && isNaN(roomShape)) {
+//     const length = parseInt(prompt("Enter length of room in feet: "));
+//     const width = parseInt(prompt("Enter width of room in feet: "));
+//     if (length > 0 && width > 0 && !isNaN(length) && !isNaN(width)) {
+//       const area = length * width;
+//       const gallons = area / 350;
+//       isNotVailad = false;
+//       alert(`You will need ${Math.ceil(gallons)} gallons of paint to cover ${area.toFixed(2)} square feet.`);
+//     } else {
+//       alert("Invalid input. Please enter positive numbers for the lengths and widths of the rectangles.");
+//     }
 
-  } else {
-    alert("Invalid shape. Please enter R for round, S for square, or L for L-shaped.");
-  }
+//   } else {
+//     alert("Invalid shape. Please enter R for round, S for square, or L for L-shaped.");
+//   }
 
-}
+// }
 
